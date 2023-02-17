40fd47f7c81a6352af438450d0e4f40e58c34b4b
drag13 <no-questions@github.noreply.com>
2023-02-17 12:59:14 +0200

    Updated fixed version

--- src/examples/cancel-effect/index.fixed.jsx
+++ src/examples/cancel-effect/index.fixed.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import style from "./index.module.css";
 const basePath = import.meta.env.VITE_API_BASE_PATH;
 
-const tabs = ["First Tab", "Second Tab"];
+const tabs = ["Dead orcs", "User Data"];
 
 export default function App() {
   const [tabName, setActiveTabName] = useState(tabs[0]);
@@ -35,21 +35,25 @@ const FirstTab = () => {
   const [counter, setCounter] = useState(0);
 
   useEffect(() => {
-    setInterval(() => {
+    const interValId = setInterval(() => {
       console.log("I am working");
       setCounter((v) => v + 1);
     }, 1000);
+
+    return () => clearInterval(interValId);
   }, []);
 
-  return <>Counter: {counter}</>;
+  return <>Dead orcs: {counter}</>;
 };
 
 const SecondTab = () => {
   const [user, setUser] = useState(null);
   useEffect(() => {
+    const abortController = new AbortController();
+
     function fetchData() {
       console.log("start loading user data");
-      fetch(`${basePath}/user/default`)
+      fetch(`${basePath}/user/default`, { signal: abortController.signal })
         .then((x) => x.json())
         .then((u) => {
           console.log("saving user to state");
@@ -58,6 +62,8 @@ const SecondTab = () => {
     }
 
     fetchData();
+
+    return () => abortController.abort();
   }, []);
 
   return <>Second tab. {user ? `Hello ${user.name}` : `loading`}</>;
